bd368402bf8205aff0890d96705fc01befa642eb
ahmednour005 <ahmednoureldeen001@gmail.com>
2020-01-22 15:41:22 +0200

    first commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyCuId-O1yamdfhHZtrr6sdl7s0685lhwkU",
+    authDomain: "online-shop-66854.firebaseapp.com",
+    databaseURL: "https://online-shop-66854.firebaseio.com",
+    projectId: "online-shop-66854",
+    storageBucket: "online-shop-66854.appspot.com",
+    messagingSenderId: "668883427883",
+    appId: "1:668883427883:web:e016a7924cb948e8fa006e",
+    measurementId: "G-8RH4KDMQQC"
+  }
 };
 
 /*
